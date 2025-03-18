2106918eaf5cfc5c3dacb151712231fefcbe4e5f
kusuma794 <kusumasri794@gmail.com>
2025-03-18 13:11:14 +0530

    updated main.tf file

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "webserver" {
-instance_type = "t2.micro"
-ami = "ami-0d5eff06f840b45e9"
+instance_type          = "t2.micro"
+ami                    = "ami-0d5eff06f840b45e9"
 vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-userdata = "${file("apache.sh")}"
+userdata               = "${file("apache.sh")}"
 tags = {
 Name = "web-server"
 }
